a4185586ee3d356ece2669170b904b8fdedcc272
taliamuro <taliamuro3@gmail.com>
2026-02-22 16:20:48 -0500

    Added customer registration form

--- app/Views/homeView.php
+++ app/Views/homeView.php
@@ -2,19 +2,41 @@
 
 use App\Helpers\ViewHelper;
 //TODO: set the page title dynamically based on the view being rendered in the controller.
-$page_title = 'Home';
+$page_title = 'Customer Registration';
 ViewHelper::loadHeader($page_title);
 ?>
 
-<h1>Slim Framework-based MVC Application</h1>
-<p>This is a simple MVC application built with Slim Framework.
-    
-</p>
-
-<p>This app uses a simple and effective way to pass the container to the controller given the small scope of the application and the fact that this application is to be used in a classroom setting where students are not yet familiar with the Dependency Inversion Principle.</p>
-
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
-<p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos. </p>
+<div class="page-content">
+    <h2>Customer Registration</h2>
+    <br>
+    <div id="form-container">
+        <form class="row g-3">
+            <div class="col-md-6">
+                <label for="fname" class="form-label">First Name</label>
+                <input type="text" class="form-control" id="fname">
+            </div>
+            <div class="col-md-6">
+                <label for="lname" class="form-label">Last Name</label>
+                <input type="text" class="form-control" id="lname">
+            </div>
+            <div class="col-12">
+                <label for="inputAddress" class="form-label">Address</label>
+                <input type="text" class="form-control" id="inputAddress">
+            </div>
+            <div class="col-12">
+                <label for="inputEmail" class="form-label">Email</label>
+                <input type="email" class="form-control" id="inputEmail">
+            </div>
+            <div class="col-12">
+                <label for="inputPhone" class="form-label">Phone Number</label>
+                <input type="email" class="form-control" id="inputPhone">
+            </div>
+            <div class="col-12">
+                <button type="submit" class="btn btn-primary">Sign in</button>
+            </div>
+        </form>
+    </div>
+</div>
 
 
 <?php
